ca3db51d4fe8bcbe92891faa63e7d742cb58eb12
Dominik <dominik.wozniewicz@gmail.com>
2021-03-22 00:59:52 +0100

    Fetched data from the EDAMAM API and assigned it to the recipes state

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Recipe from "./components/Recipe";
+
+const App = () => {
+  //TODO: secure app_id and app_key
+  const APP_ID = "80da0fec";
+  const APP_KEY = "aec5be3e0528eb278bb1ef06f8604883";
+
+  const [recipes, setRecipes] = useState([]);
+
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const fetchRecipes = async () => {
+    const { data } = await axios.get(exampleRequest);
+    setRecipes(data.hits);
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
